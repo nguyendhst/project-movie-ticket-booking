f5f55ea3b998b0f89bebc7f682f6d3cc35715037
nguyendhst <mountngubinh@gmail.com>
2022-11-05 17:15:56 +0700

    [refactor]

---
+++ src/components/footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css'
+
+function Footer() {
+    return(
+        <div className="Footer">
+                NTTVN Copyright, 2022
+        </div>
+    )
+}
+
+export default Footer;
--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React from "react"
+import './index.css';
+import HamburgerLogo from '../../Asset/Hamburger.png';
+import Footer from '../../components/footer';
+import FilmBanner from "../../components/Film_banner/FilmBanner";
 
+function Header() {
+    return(
+        <ul className="Header">
+            <ul className="Left_header">
+                <li id="Ham-icon">
+                    <img src={HamburgerLogo} alt="Hamburger icon" width="40px" height="40px"></img>
+                </li>
+                <li>
+                    <button className="CinemaLogo">
+                        Home Page
+                    </button>
+                </li>
+            </ul>
+            <li>
+                <button className="LoginButton">
+                    Đăng nhập
+                </button>
+            </li>
+        </ul>
+    )
+}
 
 const LandingPage = () => {
     return (
-        <div>
-        <h1>Landing Page</h1>
+        <div className="LandingPage">
+            <Header/>
+            <div className="LandingPage_main">
+                <h1>Welcome</h1>
+                If you can read this, KYS
+                <FilmBanner/>
+            </div>
+            <Footer/>
         </div>
-    );
+    )
 }
 
 export default LandingPage;
