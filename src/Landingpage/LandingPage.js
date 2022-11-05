63b2723a11601395e8d531fe2f1f9fd15170d91c
Thịnh Tô <todaithinh@gmail.com>
2022-11-05 16:20:33 +0700

    Adding more components

---
+++ src/Landingpage/LandingPage.js
@@ -0,0 +1,43 @@
+import React from "react"
+import './LandingPage.css';
+import HamburgerLogo from './Hamburger.png';
+import Footer from '../components/Footer/Footer';
+import FilmBanner from "../components/Film_banner/FilmBanner";
+
+function Header() {
+    return(
+        <ul className="Header">
+            <ul className="Left_header">
+                <li id="Ham-icon">
+                    <img src={HamburgerLogo} alt="Hamburger icon" width="40px" height="40px"></img>
+                </li>
+                <li>
+                    <button className="CinemaLogo">
+                        Home Page
+                    </button>
+                </li>
+            </ul>
+            <li>
+                <button className="LoginButton">
+                    Đăng nhập
+                </button>
+            </li>
+        </ul>
+    )
+}
+
+function LandingPage() {
+    return (
+        <div className="LandingPage">
+            <Header/>
+            <div className="LandingPage_main">
+                <h1>Welcome</h1>
+                If you can read this, KYS
+                <FilmBanner/>
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+
+export default LandingPage;
