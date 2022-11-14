f1f82f037acda767c5e0af4049ef0e4746f3f724
Thịnh Tô <todaithinh@gmail.com>
2022-11-14 11:54:51 +0700

    Change app.js using route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import './App.css';
-// import LandingPage from './pages/LandingPage/LandingPage';
+import {
+  Routes,
+  Route
+} from 'react-router-dom';
+
+import LandingPage from './pages/LandingPage/LandingPage';
 import Content from './pages/Content/Content';
+import TicketBooking from './pages/TicketBooking';
+import ManageShift from './pages/ManageShift';
+import CustomerCare from './pages/Customer_Care/CustomerCare';
+import NewFeedbacks from './pages/Customer_Care/NewFeedback';
 
 function App() {
   return (
-    <div className="App">
-      {/* <LandingPage /> */}
-      <Content/>
-    </div>
+    <React.Fragment>
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/content-manage" element={<Content />} />
+          <Route path="/booking" element={<TicketBooking />} />
+          <Route path="/manage-shift" element={<ManageShift />} />
+          <Route path="/customer-care" element={<CustomerCare />}>
+              <Route path='customer-care/new-feedback' element={<NewFeedbacks />} />
+          </Route>
+        </Routes>
+      </main>
+    </React.Fragment>
+    
   );
 }
 
