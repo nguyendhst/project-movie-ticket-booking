42c50fe4fe16d7bf667b9645367bd1c0703a989c
nguyennhat <mountngubinh@gmail.com>
2022-11-14 10:39:19 +0700

    Revert "Add login options and change some buttons effect, including hover and active with focus shadow"

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
+import {
+  Routes,
+  Route
+} from 'react-router-dom';
+
 import LandingPage from './pages/LandingPage/LandingPage';
+import TicketBooking from './pages/TicketBooking';
+import ManageShift from './pages/ManageShift';
+import CustomerCare from './pages/Customer_Care/CustomerCare';
+import NewFeedbacks from './pages/Customer_Care/NewFeedback';
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+    <React.Fragment>
+      <main>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/booking" element={<TicketBooking />} />
+          <Route path="/manage-shift" element={<ManageShift />} />
+          <Route path="/customer-care" element={<CustomerCare />}>
+              <Route path='customer-care/new-feedback' element={<NewFeedbacks />} />
+          </Route>
+        </Routes>
+      </main>
+    </React.Fragment>
+    
   );
 }
 
-export default App
+export default App;
