866f4a0f64bc6ae85cbf73650b70792f2b055d15
Thịnh Tô <todaithinh@gmail.com>
2022-11-16 04:57:06 +0700

    Add 2 new banner, add tag into CM

--- src/pages/Content/Content.js
+++ src/pages/Content/Content.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 import Button from "react-bootstrap/esm/Button";
+import Card from "react-bootstrap/esm/Card";
 
 import './Content.css';
 import Footer from "../../components/Footer/Footer";
-import Tabs from "../../components/Tabs/Tabs"
+import Tabs from "../../components/Tabs/Tabs";
 
-import House from "../../Asset/house.svg"
+import House from "../../Asset/house.svg";
+import Bp2 from "../../Asset/bp2.jpg";
+import banner_2 from "../../Asset/banner_2.png";
+import banner_1p_h from "../../Asset/banner_1p_h.webp"
 
 
 
@@ -34,11 +38,126 @@ function Header() {
     )
 }
 
-const Students = ["a", "b", "c", "d", ]
-const Hello = ["e", "f", "g", "h"]
+
 
 
 function Content() {
+    const OnGoing = [
+        {
+            name: "Black Panther: Wakanda Forever",
+            poster: Bp2,
+        },
+        {
+            name: "Black Panther: Wakanda Forever",
+            poster: Bp2,
+        },
+        {
+            name: "Black Panther: Wakanda Forever",
+            poster: Bp2,
+        },
+        {
+            name: "Black Panther: Wakanda Forever",
+            poster: Bp2,
+        },
+        {
+            name: "Black Panther: Wakanda Forever",
+            poster: Bp2,
+        },
+        {
+            name: "Black Panther: Wakanda Forever",
+            poster: Bp2,
+        },
+        {
+            name: "Black Panther: Wakanda Forever",
+            poster: Bp2,
+        },
+        {
+            name: "Black Panther: Wakanda Forever",
+            poster: Bp2,
+        },
+        {
+            name: "Black Panther: Wakanda Forever",
+            poster: Bp2,
+        }
+    ]
+
+    const UpComing = [
+        {
+            name: "One Piece Film: RED",
+            poster: banner_1p_h,
+        },
+        {
+            name: "One Piece Film: RED",
+            poster: banner_1p_h,
+        },
+        {
+            name: "One Piece Film: RED",
+            poster: banner_1p_h,
+        },
+        {
+            name: "One Piece Film: RED",
+            poster: banner_1p_h,
+        },
+        {
+            name: "One Piece Film: RED",
+            poster: banner_1p_h,
+        },
+        {
+            name: "One Piece Film: RED",
+            poster: banner_1p_h,
+        },
+        {
+            name: "One Piece Film: RED",
+            poster: banner_1p_h,
+        },
+        {
+            name: "One Piece Film: RED",
+            poster: banner_1p_h,
+        },
+        {
+            name: "One Piece Film: RED",
+            poster: banner_1p_h,
+        }
+    ]
+    const Archived = [
+        {
+            name: "Fight Club",
+            poster: banner_2,
+        },
+        {
+            name: "Fight Club",
+            poster: banner_2,
+        },
+        {
+            name: "Fight Club",
+            poster: banner_2,
+        },
+        {
+            name: "Fight Club",
+            poster: banner_2,
+        },
+        {
+            name: "Fight Club",
+            poster: banner_2,
+        },
+        {
+            name: "Fight Club",
+            poster: banner_2,
+        },
+        {
+            name: "Fight Club",
+            poster: banner_2,
+        },
+        {
+            name: "Fight Club",
+            poster: banner_2,
+        },
+        {
+            name: "Fight Club",
+            poster: banner_2,
+        }
+    ]
+
     return (
         <div className="Content">
             <Header/>
@@ -47,10 +166,52 @@ function Content() {
                     title={"Tab test"}
                     tabs={
                         [
-                            {name: "Student", content:  Students.map((student, index)   => <div key={index}>{(student) }</div>)},
-                            {name: "Hello", content:    Hello.map((hellu, index)        => <div key={index}>{(hellu)}</div>)},
-                            // {name: "Student2", content:  Students.map((student, index)   => <div key={index}>{(student) }</div>)},
-                            {name: "Hello2", content:    Hello.map((hellu, index)        => <div key={index}>{(hellu)}</div>)}
+                            {
+                                name: "Phim đang chiếu",
+                                content: OnGoing.map(
+                                    (film, index) =>
+                                    <Card style={{ width: '133px' }}>
+                                    <Card.Img variant="top" src={film.poster} />
+                                    <Card.Body>
+                                        <Card.Title style={{color: '#000000'}}>{film.name}</Card.Title>
+                                        {/* <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button> */}
+                                    </Card.Body>
+                                    </Card>
+                                )
+                            },
+                            {
+                                name: "Phim sắp chiếu",
+                                content: UpComing.map(
+                                    (film, index) =>
+                                    <Card style={{ width: '133px' }}>
+                                    <Card.Img variant="top" src={film.poster} />
+                                    <Card.Body>
+                                        <Card.Title style={{color: '#000000'}}>{film.name}</Card.Title>
+                                        {/* <Card.Text>
+                                        Some quick example text to build on the card title and make up the
+                                        bulk of the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button> */}
+                                    </Card.Body>
+                                    </Card>
+                                )
+                            },
+                            {
+                                name: "Phim đã dừng chiếu",
+                                content: Archived.map(
+                                    (film, index) =>
+                                    <Card style={{ width: '133px' }}>
+                                    <Card.Img variant="top" src={film.poster} />
+                                    <Card.Body>
+                                        <Card.Title style={{color: '#000000'}}>{film.name}</Card.Title>
+                                    </Card.Body>
+                                    </Card>
+                                )
+                            }
                         ]
                     }
                 />
