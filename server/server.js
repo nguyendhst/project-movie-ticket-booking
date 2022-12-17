2310d64a7ffa5d049e94ea7eeafaac0cec923429
nguyendhst <mountngubinh@gmail.com>
2022-12-17 23:07:01 +0700

    update: DB design

--- server/server.js
+++ server/server.js
@@ -35,17 +35,17 @@ try {
 }
 
 // tables in the database
-// `movies` --  (id, title, adult, poster_path, overview, genre_ids, language, release_date)
+// `movies` --  (id, title, adult, poster_path, vertical_poster_path, overview, genre_ids, language, release_date, status)
 // `genres` -- (id, name)
 
-const testQuery = (dbname) => {
+const queryGetAllMovies = (dbname) => {
   return `SELECT * FROM \`${dbname}\`.\`movies\`;`;
 };
 // Create a route
 app.get("/", async (req, res) => {
   try {
     connection.query(
-      testQuery(process.env.DB_NAME),
+      queryGetAllMovies(process.env.DB_NAME),
       function (err, results, fields) {
         if (err) {
           console.log(err);
@@ -62,6 +62,33 @@ app.get("/", async (req, res) => {
   }
 });
 
+// GET /api/movies?genre=action&language=en&sort=release_date&order=desc&limit=10&offset=0
+
+// GET /api/movies?all=true
+app.get("/api/movies", async (req, res) => {
+  const { all, genre, language, sort, order, limit, offset } = req.query;
+  if (all) {
+    try {
+      connection.query(
+        queryGetAllMovies(process.env.DB_NAME),
+        function (err, results, fields) {
+          if (err) {
+            console.log(err);
+            res.send({ error: err });
+          } else {
+            console.log(results);
+            res.send({ results });
+          }
+        }
+      );
+    } catch (e) {
+      console.log(e);
+      res.send({ error: e });
+    }
+  }
+});
+
+
 // Start the server
 try {
   app.listen(8080, () => {
@@ -74,7 +101,7 @@ try {
 
 
 // BATCH INSERTION SCRIPT
-// 
+//
 
 // const API_KEY = process.env.API_KEY;
 // const API_URL = "https://api.themoviedb.org/3/movie/";
@@ -91,37 +118,47 @@ try {
 // };
 
 // const processGenre = (genre_ids) => {
-//     const insertGenres = (gen_id, name) => {
-//         return `INSERT INTO \`${process.env.DB_NAME}\`.\`genres\` (id, name) VALUES ('${gen_id}', '${name}');`;
-//     }
-//     console.log("genre_ids", genre_ids);
-//     const data = genre_ids;
-//     try {
-//         for (const genre of data) {
-//             connection.query(
-//                 insertGenres(
-//                     genre.id,
-//                     genre.name
-//                 ),
-//                 function (err, results, fields) {
-//                     if (err) {
-//                         console.log(err);
-//                     } else {
-//                         console.log(results);
-//                     }
-//                 }
-//             );
+//   const insertGenres = (gen_id, name) => {
+//     return `INSERT INTO \`${process.env.DB_NAME}\`.\`genres\` (id, name) VALUES ('${gen_id}', '${name}');`;
+//   };
+//   console.log("genre_ids", genre_ids);
+//   const data = genre_ids;
+//   try {
+//     for (const genre of data) {
+//       connection.query(
+//         insertGenres(genre.id, genre.name),
+//         function (err, results, fields) {
+//           if (err) {
+//             console.log(err);
+//           } else {
+//             console.log(results);
+//           }
 //         }
-//     } catch (e) {
-//         console.log(e);
+//       );
 //     }
+//   } catch (e) {
+//     console.log(e);
+//   }
 
-//     // return array of ids as string
-//     return data.map((genre) => genre.id).join(",");
-// }
+//   // return array of ids as string
+//   return data.map((genre) => genre.id).join(",");
+// };
 
 // const escape = (str) => {
 //   return str.replace(/'/g, "\\'");
+// };
+
+// const customiseStatus = (status) => {
+//   console.log("[][][]", status);
+//   if (status === "Released") {
+//     return 1;
+//   } else if (status === "Rumored") {
+//     return 2;
+//   } else if (status === "Post Production") {
+//     return 3;
+//   } else {
+//     return 4;
+//   }
 // };
 
 // // insert
@@ -130,17 +167,18 @@ try {
 //   title,
 //   adult,
 //   poster_path,
+//   vertical_poster_path,
 //   overview,
 //   genre_ids,
 //   language,
-//   release_date
+//   release_date,
+//   status
 // ) => {
-//   return `INSERT INTO \`${dbname}\`.\`movies\` (title, adult, poster_path, overview, genre_ids, language, release_date) VALUES ('${title}', '${adult}', '${poster_path}', '${overview}', '${genre_ids}', '${language}', '${release_date}');`;
+//   return `INSERT INTO \`${dbname}\`.\`movies\` (title, adult, poster_path, vertical_poster_path, overview, genre_ids, language, release_date, status) VALUES ('${title}', '${adult}', '${poster_path}', '${vertical_poster_path}', '${overview}', '${genre_ids}', '${language}', '${release_date}', '${status}');`;
 // };
-
 // // JUST EDIT THIS LINE
 // const ids = [1045944, 19995, 550, 505642, 436270, 736526, 724495, 76600, 873126];
-
+// // const ids = [1045944];
 // const insert = async (ids) => {
 //   try {
 //     for (const id of ids) {
@@ -149,12 +187,14 @@ try {
 //         insertQuery(
 //           process.env.DB_NAME,
 //           movie.title,
-//           "0",
+//           movie.adult ? 1 : 0,
 //           escape(movie.poster_path),
+//           escape(movie.backdrop_path),
 //           escape(movie.overview),
 //           processGenre(movie.genres),
 //           movie.original_language,
-//           movie.release_date
+//           movie.release_date,
+//           customiseStatus(movie.status)
 //         ),
 //         function (err, results, fields) {
 //           if (err) {
--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -8,30 +8,28 @@ CREATE DATABASE IF NOT EXISTS movie_db;
 -- Use the database
 USE movie_db;
 
--- Create the table
+-- Create movies table
+DROP TABLE IF EXISTS movies;
 CREATE TABLE IF NOT EXISTS movies (
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(255) NOT NULL,
   adult BOOLEAN NOT NULL,
   poster_path VARCHAR(255) NOT NULL,
+  vertical_poster_path VARCHAR(255) NOT NULL,
   overview TEXT NOT NULL,
   genre_ids VARCHAR(255) NOT NULL,
   language VARCHAR(255) NOT NULL,
   release_date DATE NOT NULL,
+  status ENUM('Trending', 'Ongoing', 'Upcoming', 'Archived') NOT NULL,
   
   PRIMARY KEY (id)
 );
 
-
 -- Genre table
+DROP TABLE IF EXISTS genres;
 CREATE TABLE IF NOT EXISTS genres (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(255) NOT NULL,
   
   PRIMARY KEY (id)
 );
--- Insert some data
-
-INSERT INTO movies (title, adult, poster_path, overview, genre_ids, language, release_date) VALUES
-('Fight Club', 0, '/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg', '"A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground \"fight clubs\" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion."', 
-'18;53;35', 'en', '1999-10-15');
