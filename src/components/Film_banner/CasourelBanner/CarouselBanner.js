52294ae7ec14f11083d6f235055da4eb10cff7ff
VuTran2601 <nguyenvutran2601@gmail.com>
2022-12-31 10:31:27 +0700

    Update CarouselBanner.js

--- src/components/Film_banner/CasourelBanner/CarouselBanner.js
+++ src/components/Film_banner/CasourelBanner/CarouselBanner.js
@@ -1,40 +1,38 @@
-import React, { useState } from "react";
-import Carousel from "react-bootstrap/esm/Carousel";
-import "./CarouselBanner.css";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/esm/Carousel';
+import './CarouselBanner.css';
 
-// import films from "../../../data/films.json";
+import films from '../../../data/films.json'
 
-const imgPath = "https://image.tmdb.org/t/p/original";
-const moviePath = "http://localhost:3000/movie/";
-
-function CarouselBanner(props) {
-    const { trending } = props;
+function CarouselBanner() {
     const [index, setIndex] = useState(0);
 
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
 
-  let trendFilms = films.Trend;
-  let OnGoingFilms = films.OnGoing;
-  return (
-    <Carousel className="Banner_vertical" activeIndex={index} onSelect={handleSelect}>
-      {trendFilms.map((trend) => 
-      OnGoingFilms.filter((Ongoing) => Ongoing.id.toString() === trend.toString()).map(
-        (trendFilms) =>
-        // console.log(trendFilms)
-          <Carousel.Item> 
-          <a href={"/movie/" + trendFilms.id}> 
-            <img
-              className='d-block w-100 rounded'
-              src={trendFilms.verticalPoster}
-              alt="Film slide"
-            />
-            </a>
-          </Carousel.Item>
-      )
-      // console.log(trend)
-      )}
-    </Carousel>
-  );
+    let trendFilms = films.Trend;
+    let OnGoingFilms = films.OnGoing;
+    return (
+        <Carousel className="Banner_vertical" activeIndex={index} onSelect={handleSelect}>
+            {trendFilms.map((trend) =>
+                    OnGoingFilms.filter((Ongoing) => Ongoing.id.toString() === trend.toString()).map(
+                        (trendFilms) =>
+                            // console.log(trendFilms)
+                            <Carousel.Item>
+                                <a href={"/movie/" + trendFilms.id}>
+                                    <img
+                                        className='d-block w-100 rounded'
+                                        src={trendFilms.verticalPoster}
+                                        alt="Film slide"
+                                    />
+                                </a>
+                            </Carousel.Item>
+                    )
+                // console.log(trend)
+            )}
+        </Carousel>
+    );
 }
 
 // render(<CarouselBanner />);
